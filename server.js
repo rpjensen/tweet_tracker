717d06a1f84e7c2dddf080be11837e151e3a7fc2
Ryan Jensen <rjensen.professional@gmail.com>
2015-03-24 00:13:28 -0400

    Finished project for now

---
+++ server.js
@@ -0,0 +1,216 @@
+(function () {
+	//"use strict";
+	
+	var express = require("express"),
+			http = require("http"),
+			app = express(),
+			redis = require('node-redis'),
+			client = redis.createClient(6379, '127.0.0.1'),
+			ntwitter = require('ntwitter'),
+			credentials = require('./credentials.json'),
+			twitter = ntwitter(credentials);
+
+
+	app.use(express.static(__dirname));
+	http.createServer(app).listen(3000);
+	
+	console.log("before");
+
+	//console.log(client);
+	/*client.hmset("hosts", "mjr", "1", "another", "23", "home", "1234");
+	var thing 
+client.hgetall("hosts", function (err, obj) {
+    console.log(obj);
+	for (var i = 0; i < obj.length; i++) {
+		console.log(obj[i]);
+		console.log(obj[i].toString());
+	}
+});*/
+	/*
+	client.hmset("1", "filter","climate change", "tweet","climate change is bad mkay");
+	//console.log(client);
+	console.log("after");
+	client.hgetall("1", function (err, object) {
+					if (err) {
+						console.log("Error getting tweet " + keys[i]);
+					}
+					else if (object) {
+						//var javaObject = JSON.parse(object);
+						var arr = object.toString().split(",");
+						var object = {};
+						var lastKey;
+						for (var i = 0; i < arr.length; i++) {
+							if (i % 2 === 0) {
+								lastKey = arr[i];
+								object[lastKey] = arr[i+1];
+							}
+						}
+						console.log(arr);	
+						console.log(object);
+					}
+	});
+	*/
+	// add a tweet
+	var addTweet = (function() {
+		// the current insert id
+		var count = 0;
+		// the actual add tweet function
+		return function (filter, text) {
+			//console.log("filter: " + filter + ", tweet: " + text);
+			console.log("add tweet called");
+			count++;// inc the count
+			// a tweet has a filter and the text
+			//var tweet = {"filter" : filter, "tweet" : text};
+			//var tweet = [count, "filter", filter, "tweet", text];
+			//console.log(tweet);
+			client.hmset(count, "filter", filter, "tweet", text);// the key is just the count
+			client.expire(count, 10);// set the expiration time for the tweet
+			
+		};
+	}());
+
+	// get the list of filters
+	var getFilters = function(req, res) {
+		res.send(filters);
+	};
+
+	// a function to get all the current tweets
+	var getTweets = function(req, res) {
+		console.log("get tweets started");
+		
+		
+		// get all the keys
+		var callback = function (keys) {
+			
+			console.log("callback got keys");
+			var updates = {counts : [], tweets : []};
+			// create the container for the tweets we fetch
+			for (var i = 0; i < filters.length; i++) {
+				updates.counts.push({filter : filters[i], count : 0});
+			} 
+
+			var waiting = keys.length;
+			var getDone = function() {
+				waiting--;
+				if (waiting <= 0) {
+					updates.tweets.sort(function(a, b) {
+						// a < b => -, a > b => +, a = b => 0
+						return parseInt(a.filter) - parseInt(b.filter);
+					});
+					res.send(updates);	
+				}
+			};
+			// for each key get the hash set that contains the tweet
+			for (var i = 0; i < keys.length; i++) {
+				// get the hash object for that key
+				console.log("calling hgetall on key " + keys[i]);
+				client.hgetall(keys[i], function (err, object) {
+					console.log("inside hgetall for key " + keys[i]);
+					if (err) {
+						console.log("Error getting tweet " + keys[i]);
+					}
+					else if (object) {
+						/*console.log(object.keys());
+						console.log(object.toString());
+						var arr = object.toString().split(",");*/
+						var arr = [];
+						for (var j = 0; j < object.length; j++) {
+							arr.push(object[j].toString());	
+						}
+						console.log(object);
+						console.log(arr);
+						var object = {};
+						var lastKey;
+						for (var j = 0; j < arr.length; j++) {
+							if (j % 2 === 0) {
+								lastKey = arr[j];
+								object[lastKey] = arr[j+1];
+							}
+						}	
+						console.log(object);
+						updates.tweets.push(object);
+						for (var j = 0; j < updates.counts.length; j++) {
+							if (updates.counts[j].filter === object.filter) {
+								updates.counts[j].count++;
+							}
+						}
+					}
+					getDone();
+				});
+			}
+console.log("waiting");
+		/*	while(waiting > 0) {
+				// empty loop while we wait for async calls
+				// obviously not the most efficient way to do this
+			}
+			process.nextTick(function() {
+				if (waiting > 0) {
+					process.nextTick(arguments.callee);
+				}
+			});
+			*/
+			
+			if (waiting <= 0) {
+				console.log("callback called");
+				callback(updates);	
+			}
+	
+			console.log("made it past waiting");
+
+			
+			//res.send(updates);
+		};
+		client.keys('*', function (err, keys) {
+			console.log(keys.toString());
+			var parsedKeys = keys.toString().split(",");
+			console.log(parsedKeys);
+			callback(parsedKeys);
+		});
+		
+	};
+	
+	console.log(getTweets);
+	app.get("/getTweets", getTweets);
+	app.get("/getFilters", getFilters);
+
+	// the filters to track tweets of
+	var filters = ["dark matter", "homeopathy", "global warming", "climate change", "vaccine", "gmo"];
+	var splitFilters = [];// the filters separated by whitespace
+	var allFilters = [];// all the filters (used to get all possible tweets from the twitter api)
+	for (var i = 0; i < filters.length; i++) {
+		splitFilters.push(filters[i].split(" "));
+		for (var j = 0; j < splitFilters[i].length; j++) {
+			allFilters.push(splitFilters[i][j]);
+		}
+	}
+
+	// track tweets from twitter
+	twitter.stream(
+		"statuses/filter",
+		{"track": allFilters},
+		function(stream) {
+			stream.on("data", function(tweet) {
+				// for each filter
+				//console.log(tweet.text);
+				for (var i = 0; i < splitFilters.length; i++) {
+					// if the match falls through and is still true we have a full match
+					var match = true;
+					for (var j = 0; j < splitFilters[i].length; j++) {
+						// if a word for this filter doesn't match the whole thing doesn't match
+						if (tweet.text.indexOf(splitFilters[i][j]) === -1) {
+							match = false;
+							break;
+						}
+					}
+					if (match) {
+						// add the tweet and matching filter
+						addTweet(filters[i], tweet.text);
+					}
+				}
+			});
+		}
+	);
+
+}());
+
+
--- tweets.js
+++ tweets.js
@@ -3,7 +3,7 @@
 	"use strict";
 
 	// Extend the jquery library to scroll the caller(s) to the given element
-	jQuery.fn.scrollTo = function(elem, speed) { 
+	jQuery.fn.scrollTo = function (elem, speed) {
 		// If this is a collection recursively call it on its children
 		if (this.length > 1) {
 			for (var i = 0; i < this.length; i++) {
@@ -28,9 +28,59 @@
 
 	// Main method, called once on load
 	var main = function() {
-		$('.graph__bar').on('click', function() {
-			$(this).animate({height: $(this).height() + 10});
-		})
+		
+		var timer;
+
+		var fetchUpdates = function () {
+			console.log("fetch updates called");
+			var resource = '/getTweets';
+			var data = {};
+			/*
+			$.each(currentLabels, function(index, value) {
+				data['label'+index] = value;
+			});
+			*/
+
+			$.get(resource, data, function (responseObject) {
+				console.log(responseObject);
+				//var responseObject = JSON.parse(reply);
+				console.log(responseObject);
+				var counts = responseObject.counts;
+				tweetTracker('updateCounts')(counts);
+				var tweets = responseObject.tweets;
+				tweetTracker('updateTweets')(tweets);
+			});
+			timer = setTimeout(fetchUpdates, 10000);
+		};
+
+		var currentLabels = [];
+
+		$.get('/getFilters', {}, function (responseObject) {
+			//var responseObject = JSON.parse(reply);
+			tweetTracker('addFilters')(responseObject);
+		});
+
+		$('#start-tracking').on('click', function () {
+			/*
+			$('.label-input__widget').each(function (index, input) {
+				var prefix = 'label-input__widget-';
+				var id = parseInt(input.id.substring(prefix.length)) - 1;
+				currentLabels[id] = $(input).val();
+			});
+			*/
+			$('#stop-tracking').prop('disabled', false);
+			$(this).prop('disabled', true);
+
+			fetchUpdates();
+		});
+		$('#stop-tracking').on('click', function () {
+			clearTimeout(timer);
+			$('#start-tracking').prop('disabled', false);
+			$(this).prop('disabled', true);
+		});
+
+		fetchUpdates();
+		$('#start-tracking').prop('disabled', true);
 		
 	};
 
@@ -41,16 +91,17 @@
 
 /**
 * Tweet tracker holds a list of tweet filters
-* 
+* Filters can be reset, added, updated, and their labels can be fetched using the messages
+* resetFilters, addFilter, update, and getLabels
 */
 var tweetTracker = (function() {
 	"use strict";
-	var newTweetLabel =  function() {
+	// Constructor function for a new tweet filter
+	var newTweetFilter =  function() {
+		// tweet filter ivars
 		var label = '';
 		var count = 0;
-		var callbackG = function() {};
 		var tweets = [];
-		var callbackT = function() {};
 		return function(msg) {
 			// set the label for the tweet filter
 			if (msg === 'setLabel') {
@@ -64,14 +115,6 @@ var tweetTracker = (function() {
 					return label;
 				};
 			}
-			// Set the callback for the count
-			if (msg === 'setGraphCallback') {
-				return function(value) {
-					if (typeof value == 'function') {
-						callbackG = value;
-					}
-				};
-			}
 			// get the current count
 			if (msg === 'getCount') {
 				return function() {
@@ -83,7 +126,6 @@ var tweetTracker = (function() {
 				return function(value) {
 					if (typeof value == "number") {
 						count = value;
-						callbackG(value);
 					}
 				};
 			}
@@ -92,7 +134,6 @@ var tweetTracker = (function() {
 				return function(value) {
 					if (typeof value == "number") {
 						count += value;
-						callbackG(count);
 					}
 				};
 			}
@@ -100,15 +141,6 @@ var tweetTracker = (function() {
 			if (msg === 'addTweet') {
 				return function(tweet) {
 					tweets.push(tweet);
-					callbackT(tweet);
-				};
-			}
-			// Set the tweet callback
-			if (msg === 'setTweetCallback') {
-				return function(value) {
-					if (typeof value == "function") {
-						callbackT = value;
-					}
 				};
 			}
 			// get the current list of tweets
@@ -119,44 +151,68 @@ var tweetTracker = (function() {
 			}
 		};
 	};
+
+	// Tweet tracker ivars
 	var tweetLabels = [];
+	var tweets = [];
+	var updateRate = 10000;
 
 	return function(msg) {
 		// Reset the filters
-		if (msg === "resetLabels") {
+		if (msg === "resetFilters") {
 			tweetLabels = [];
+			tweets = [];
+			tweetGraph('resetGraph')();
+			tweetFeed('clearTweets')();
 		}
 		// Add a filter
-		if (msg === "addLabel") {
-			return function(label, callbackGraph, callbackFeed) {
-				// create a new tweet label
-				var tl = newTweetLabel()
-				tl('setLabel')(label);// set the label name
-				tl('setGraphCallback')(callbackGraph);// set the graph callback
-				tl('setTweetCallback')(callbackFeed);// set the tweet callback
-				tweetLabels.push(tl);// add it to the list
+		if (msg === "addFilters") {
+			// Prams: label - the label for the filter, 
+			return function(labels) {
+				for (var i = 0; i < labels.length; i++) {
+					// create a new tweet label
+					var tl = newTweetFilter()
+					tl('setLabel')(labels[i]);// set the label name
+					tweetLabels.push(tl);// add it to the list
+				}
+				tweetGraph('setLabels')(labels);
 			};
 		}
-		// Update Tweets and counts {key: "label", count : 5, tweets : [t1, t2, ...]}
-		if (msg === "update") {
+		// Update Counts Format: [{filter: "label", count : 5}, {...}, ...]
+		if (msg === "updateCounts") {
 			return function(updates) {
 				// for each update
 				for (var i = 0; i < updates.length; i++) {
 					var update = updates[i];// get the update
 					// for each tweet filter
 					for (var j = 0; j < tweetLabels.length; j++) {
-						// check if the update key is the same as the tweet filter label
-						if (update.key === tweetLabels[j]('getLabel')()) {
+						// check if the update filter is the same as the tweet filter label
+						if (update.filter === tweetLabels[j]('getLabel')()) {
 							// update the count
 							tweetLabels[j]('addCount')(update.count);
-							// update the tweets
-							for (var k = 0; k < update.tweets.length; k++) {
-								tweetLabels[j]('addTweet')(update.tweets[k]);
-							}
+							tweetGraph('updateCount')(update.filter, tweetLabels[j]('getCount')());
 						}
 					}
 				}
 			};
+		}
+		// Update tweets in the format: [{filter: "label", tweet: "Hello, World"}, {...}, ...]
+		// The tweets should be in order from oldest to newest
+		if (msg === 'updateTweets') {
+			return function(tweetList) {
+				for (var i = 0; i < tweetList.length; i++) {
+					var current = tweetList[i];
+					tweets.push(current.tweet);
+					tweetFeed('addTweet')(current.tweet);
+					for (var j = 0; j < tweetLabels.length; j++) {
+						// check if the tweet key is the same as the tweet filter label
+						if (current.filter === tweetLabels[j]('getLabel')()) {
+							// update the tweet
+							tweetLabels[j]('addTweet')(current.tweet);
+						}
+					}
+				}
+			}
 		}
 		// Get an array of all the current labels
 		if (msg === "getLabels") {
@@ -165,9 +221,256 @@ var tweetTracker = (function() {
 				for (var i = 0; i < tweetLabels.length; i++) {
 					list.push(tweetLabels("getLabel")());
 				}
-			}
+				return list;
+			};
+		}
+		// get the update rate
+		if (msg === 'getUpdateRate') {
+			return function() {
+				return updateRate;
+			};
+		}
+		// filter tweets based on a given label
+		if (msg === 'filterTweets') {
+			return function(label) {
+				// reset the current tweet list
+				tweetFeed('clearTweets')();
+				if (!label) {
+					// add all tweets (no filter)
+					tweetFeed('addTweets')(tweets);
+					$('.selected-filter__label').text('(all)');
+				}
+				else {
+					$('.selected-filter__label').text('(' + label + ')');
+					// find the given label and get its tweets
+					for (var i = 0; i < tweetLabels.length; i++) {
+						if (label === tweetLabels[i]('getLabel')()) {
+							tweetFeed('addTweets')(tweetLabels[i]('getTweets')());
+						}
+					}
+				}
+			};
 		}
 	};
 
 }());
+
+
+var tweetGraph = (function() {
+
+	var bars = [];
+	var $bars = $('.graph__bar');
+	var initCount = 10;
+	var maxCount = initCount;
+
+	var graphHeight = $('.graph').height();
+	var idPrefix = '#graph__bar-';
+	var updateRate = Math.floor(tweetTracker('getUpdateRate')() * 0.6);
+	var labelRate = updateRate / 2;
+
+	// add an onclick listener to the bars
+	$('.graph__bar').on('click', function() {
+		if ($(this).hasClass('selected-bar')) {
+			// if this bar was already selected de-select it
+			$(this).removeClass('selected-bar');
+			// filter by all
+			tweetTracker('filterTweets')();
+		}
+		else {
+			// else de-select any previously selected bars
+			for (var i = 0; i < $bars.length; i++) {
+				if ($($bars[i]).hasClass('selected-bar')) {
+					$($bars[i]).removeClass('selected-bar');
+				}
+			}
+			// select this bar
+			$(this).addClass('selected-bar');
+			// get its matching label and filter by that label
+			var num = $(this).attr('id').substring('graph__bar-'.length);
+			var labelId = '#x-axis__label-' + num;
+			var label = $(labelId).text();
+			tweetTracker('filterTweets')(label);
+		}
+	});
+	
+	var updateAxis = function() {
+		var prefix = 'y-axis__label';
+		// get all the axis labels
+		var $axisLabels = $('.'+prefix);
+		for (var i = 0; i < $axisLabels.length; i++) {
+			// parse the number of the axis (between 1 - 5)
+				
+			// fade out the old label and fade in the new label
+			$($axisLabels[i]).fadeOut(1000, function() {
+				var axisNum = parseInt(this.id.substring(prefix.length+1));
+				// get the new label
+				var count = Math.floor(axisNum * maxCount / 5);
+				console.log(count);
+				$(this).text(count).fadeIn(1000);
+			});
+		}
+	};
+
+	var updateGraph = function() {
+		updateAxis();
+		// resize the bars at a higher rate
+		var oldUpdateRate = updateRate;
+		updateRate = 1000;
+		for (var i = 0; i < bars.length; i++) {
+			// force the bars to resize with the new max count
+			tweetGraph('updateCount')(bars[i].label, bars[i].count);
+		}
+		updateRate = oldUpdateRate;
+		/*
+		$('#test').fadeOut(1000, function() {
+	        $(this).text('Some other text!').fadeIn(1000);
+	    });*/
+	};
+
+	updateAxis();
+
+	return function(msg) {
+		if (msg === 'updateCount') {
+			return function(label, count) {
+				var highest = 0;
+				// for each bar look for the one with the matching label
+				for (var i = 0; i < bars.length; i++) {
+					if (bars[i].label === label) {
+						// update the count
+						bars[i].count = count;
+						// get the px height using the (px/count) ratio of the graph
+						var barHeight = graphHeight / maxCount * count;
+						var barId = idPrefix + (i+1);// get the bars id
+						$(barId).animate({height: barHeight}, updateRate);// animate the height change
+						var labelSel = barId + ' .graph__bar__label';// get the label that decends from this bar (note the space)
+
+						//$(labelSel).text(count);// set the labels count
+						if (barHeight > 25) {
+							$(labelSel).fadeOut(labelRate, function() {
+								$(labelSel).text(count).fadeIn(labelRate);
+							});
+						}
+						else {
+							$(labelSel).text('');
+						}
+						
+						// if the bar is too small don't show text
+						if (barHeight < 15) {
+							$(labelSel).text('');
+						}
+					}
+					// find the highest of all the bars
+					if (bars[i].count > highest) {
+						highest = bars[i].count;
+					}
+				}
+				// if the highest is getting near the top of the graph make it bigger
+				if (highest > (maxCount * 0.9)) {
+					maxCount = Math.floor(maxCount * 1.5);
+					// Round maxCount to the next multiple of 5 so the axis labels will be clean be default
+					maxCount += (5 - maxCount % 5) % 5;
+					updateGraph();
+				}
+			};
+		}
+		if (msg === 'resetGraph') {
+			return function() {
+				for (var i = 0; i < bars.length; i++) {
+					tweetGraph('updateCount')(bar.label, 0);
+				}
+				$('.x-axis__label').text('');
+				bars = [];
+				maxCount = initCount;
+			};
+		}
+		if (msg === 'setLabels') {
+			return function(labels) {
+				for (var i = 0; i < labels.length; i++) {
+					var idNum = (i+1);
+					var idSel = '#x-axis__label-' + idNum;
+					console.log(idSel);
+					console.log($(idSel));
+					$(idSel).text(labels[i]);
+					bars.push({label: labels[i], count: 0});
+				}
+			};
+		}
+	};
+}());
+
+var tweetFeed = (function() {
+	var tweets = [];
+	var tweetElements = [];
+	var $tweetAnchor = $('.twitter-feed');
+	var $scrollAnchor = $('.feed__area');
+	var selectedTweet = null;
+
+	// create a new html element for the tweet
+	var getTweetHtml = function(content, idNum) {
+		var id = 'tweet-id-' + idNum;// get the id tag
+		// make a tweet-box div and add an onclick handler
+		var html = $('<div>').addClass('tweet-box').attr('id', id).on('click', function() {
+			if ($(this).hasClass('selected-tweet')) {
+				$(this).removeClass('selected-tweet');
+				selectedTweet = null;
+			}
+			else {
+				focusOnTweet(html, true);
+			}
+		});
+		// append a new tweet with the text
+		html.append($('<p>').addClass('tweet').text(content));
+		return html;
+	};
+
+	// focus on a given tweet element
+	// if click is true it changes the class and doesn't animate 
+	var focusOnTweet = function(tweet, click) {
+		if (selectedTweet && $(selectedTweet).hasClass('selected-tweet')) {
+			$(selectedTweet).removeClass('selected-tweet');
+		}
+		selectedTweet = tweet;
+		if (click) {
+			$(selectedTweet).addClass('selected-tweet');
+			$scrollAnchor.scrollTo(tweet);
+		}
+		else {
+			$scrollAnchor.scrollTo(tweet, 500);
+		}
+	};
+
+	var addTweet = function(tweet) {
+		tweets.push(tweet);
+		var tweetHtml = getTweetHtml(tweet, tweets.length);
+		$tweetAnchor.append(tweetHtml);
+		if (selectedTweet) {
+			focusOnTweet(selectedTweet, true);
+		}
+		else {
+			focusOnTweet(tweetHtml, false);
+		}
+	};
+
+	return function(msg) {
+		if (msg === 'addTweet') {
+			return addTweet;
+		}
+		if (msg === 'addTweets') {
+			return function(tweetList) {
+				for (var i = 0; i < tweetList.length; i++) {
+					addTweet(tweetList[i]);
+				}
+			};
+		}
+		if (msg === 'clearTweets') {
+			return function() {
+				tweets = [];
+				$tweetAnchor.empty();
+			};
+		}
+	};
+
+}());
+
+
 
