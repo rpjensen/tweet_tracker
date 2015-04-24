635f6e26fde66775c36a01707959ff4e55353ba5
Ryan Jensen <rjensen.professional@gmail.com>
2015-04-24 12:14:08 -0400

    Changed password

--- server.js
+++ server.js
@@ -12,7 +12,7 @@
 	var url = require('url');
 	var redisURL = url.parse(process.env.REDISCLOUD_URL);
 	var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
-	client.auth(redisURL.auth.split(":")[1]);
+	client.auth("rjensen");
 
 
 	app.use(express.static(__dirname));
