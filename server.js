2b2dfa30031ecc5e7ffcbbe7332d60c936f8a7c8
Ryan Jensen <rjensen.professional@gmail.com>
2015-04-24 18:08:09 -0400

    changed position of auth

--- server.js
+++ server.js
@@ -12,8 +12,9 @@
 	var url = require('url');
 	var redisURL = url.parse(process.env.REDISCLOUD_URL);
 	var client = redis.createClient(redisURL.port, redisURL.hostname);//, {no_ready_check: true}
-	console.log(client);
 	client.auth(redisURL.auth.split(":")[1]);
+	console.log(client);
+	
 
 
 	app.use(express.static(__dirname));
